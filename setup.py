3c450325faa38edf0ea161376754c1a991f468ab
Knut Andreas Kvåle <knut.a.kvale@gmail.com>
2022-07-18 08:00:00 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="pycellar",
-    version="0.2.0",
+    version="0.2.1",
     author="Knut Andreas Kvåle",
     author_email="knut.a.kvale@gmail.com",
     description="Python wine cellar library management",
